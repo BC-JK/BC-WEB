65660509bd9ee671462c85f10c731ac797c8f7db
BC-JK <jurien@brushcom.net>
2025-07-08 14:12:47 +0200

    Create plyr-init.js

---
+++ Website/plyr-init.js
@@ -0,0 +1,10 @@
+const player = new Plyr('#player', {
+      controls: [],
+      muted: true,
+      loop: true,
+      vimeo: {
+        autoplay: true,
+        muted: true,
+        loop: true,
+      },
+     });
